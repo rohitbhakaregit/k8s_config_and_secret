ca65b7b315000dd7f6e259546b4ab727e5596d42
rohitbhakaregit <bhakarerohit@gmail.com>
2020-03-26 14:33:39 +0530

    removed unwanted files

--- t.sh
+++
@@ -1,7 +0,0 @@
-echo "# k8s_config_and_secret" >> README.md
-git init
-git add README.md
-git add .
-git commit -m "first commit"
-git remote add origin https://github.com/rohitbhakaregit/k8s_config_and_secret.git
-git push -u origin master
