954dac4a75c3aa318d897a9ba60c12301c90ca04
rohitbhakaregit <bhakarerohit@gmail.com>
2020-03-26 14:33:11 +0530

    first commit

---
+++ t.sh
@@ -0,0 +1,7 @@
+echo "# k8s_config_and_secret" >> README.md
+git init
+git add README.md
+git add .
+git commit -m "first commit"
+git remote add origin https://github.com/rohitbhakaregit/k8s_config_and_secret.git
+git push -u origin master
